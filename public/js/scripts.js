b30bdcf8375af8a872d6a242912b031b16a77c94
Santiago Argilla <sargilla@gmail.com>
2022-09-05 11:07:42 -0300

    scripts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,13 @@
 window.addEventListener("load", function () {
   /*  Animations initialization */
   new WOW().init();
+
+  /* Toastr Initialization */
+  toastr.options = {
+    positionClass: "toast-bottom-right",
+    fadeIn: 300,
+    fadeOut: 1000,
+    timeOut: 5000,
+    extendedTimeOut: 1000,
+  };
 });
